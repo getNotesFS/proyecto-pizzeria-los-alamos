fbd69457e4481e1d722c79c31412be69f089f2ba
getNotesFS <sfmarmolg@gmail.com>
2020-11-13 21:19:15 -0500

    Âdfsdfs

--- app_server/routes/index.js
+++ app_server/routes/index.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 /*AQUI VAN LAS RUTAS*/
 
 const express = require('express');
@@ -118,4 +119,6 @@ router.post('/pizza/new',ctrlLocations.addNewPizza);
 module.exports = router;
 
  
+=======
+>>>>>>> 3ea0ec8d86e038494eaa3c8c8ad6f776116ac3d5
 
