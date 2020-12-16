fa691c734315c4c2d6bbee702f986a3f67688de9
Josue <josue_rap@hotmail.com>
2020-12-16 12:31:59 -0500

    Merge branch 'main' of https://github.com/getNotesFS/proyecto-pizzeria-los-alamos into main

---
+++ app-public/src/app/offerts-data.service.ts
@@ -0,0 +1,30 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';//1. Importar el servicio HTTP dentro del archivo de servicios
+import { Ofertas } from './ofertas/ofertas.component'; //6. Importar la clase Ofertas
+
+@Injectable({
+  providedIn: 'root'
+})
+export class OffertsDataService {
+
+  constructor(private http: HttpClient) { } //2.Inyectar el servicio HTTPClient dentro de nuestro servicio
+  
+  private apiBaseUrl = 'http://localhost:3000/api/ofertas';
+
+  //5. Metodo Publico para que el componente pueda llamarlo. Devuelve una Promesa (devuelve todo lo que esta en la base de datos)
+  public getOfferts(): Promise<Ofertas> {
+    
+    const codigo: string = '5fd9c4863c7a6b283833ccc1'; //id
+    const url: string = `${this.apiBaseUrl}/${codigo}`;
+    return this.http
+      .get(url)
+      .toPromise()
+      .then(response => response as Ofertas[])
+      .catch(this.handleError);
+  }
+
+  private handleError(error: any): Promise<any> {
+    console.error('Error en la lectura de ofertas', error);
+    return Promise.reject(error.message || error);
+  }
+}
